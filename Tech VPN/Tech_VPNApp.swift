69c3662f8426b8ce78d901492548b3d10dc126f2
Yusuf Alam <business@xylosolution.com>
2026-03-20 03:09:07 +0530

    Initial Commit

---
+++ Tech VPN/Tech_VPNApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tech_VPNApp.swift
+//  Tech VPN
+//
+//  Created by Xylo on 20/03/26.
+//
+
+import SwiftUI
+
+@main
+struct Tech_VPNApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
